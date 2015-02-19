75aa7b23506da78e9b5866927ebcd9d1ac00390c
cynicalanlz <ds@kelnik.ru>
2015-02-19 11:51:05 +0300

    upd

--- libs/analytics_init_domspbren.js
+++ libs/analytics_init_domspbren.js
@@ -131,7 +131,9 @@ $LAB
         config.expVar = cxApi.chooseVariation();
         cxApi.setChosenVariation(config.expVar, config.expId);                    
         tracker.set('expId', config.expId);
-        tracker.set('expVar', config.expVar);        
+        tracker.set('expVar', config.expVar); 
+        tracker.set('dimension5', [config.expVar, config.expId].join("--"));
+        console.log(tracker.get('dimension5'));
         ga(config.tracker_name + ".require", "displayfeatures");
         ga(config.tracker_name + ".require", "Monster", config);
         ga(config.tracker_name + ".require", "GA_data", config);
