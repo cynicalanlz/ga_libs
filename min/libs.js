79fda3f53ffde46a584180f9c7e200016caf9df2
cynicalanlz <adsnsrg@gmail.com>
2015-11-06 12:53:30 +0300

    referal and path dimensions

--- min/libs.js
+++ min/libs.js
@@ -310,6 +310,7 @@ define("libs", ["__postload"], function(t) {
 		var e = ["#", (window.location.hash || "#").split("#")[1].split("/")[0], t].join("");
 		window._monsterHash = t, window.location.replace(window.location.href.split("#")[0] + e);
 		console.log(e);
+		console.log(q);
 		q.loc.hash = e;
 		this.loc.hash = e;
 
